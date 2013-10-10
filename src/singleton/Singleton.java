9706e122235d5b3f3b9e9a82f41ad8296071449d
Kane.Sun <taojintianxia@126.com>
2013-10-10 16:14:13 +0800

    add comments for singleton

--- src/singleton/Singleton.java
+++ src/singleton/Singleton.java
@@ -10,6 +10,11 @@ package singleton;
 
 public class Singleton {
 
+	/*
+	 * to get more info , just check at here
+	 * http://blog.csdn.net/cnyyx/article/details/7482735
+	 */
+
 	// the volatile keyword ensures that multiple threads handle the
 	// uniqueInstance variable correctly when it is being initialized to the
 	// Singleton instance
