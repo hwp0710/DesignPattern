718cc788aab22036a523c37e41cd67de0dbeb111
Kane.Sun <taojintianxia@126.com>
2014-07-01 08:40:54 +0800

    format code

--- src/pattern/decorator/Decorator.java
+++ src/pattern/decorator/Decorator.java
@@ -1,25 +1,24 @@
 package pattern.decorator;
 
-
 /**
  *
  * @author Kane.Sun
  */
-public class Decorator implements SourceInterface{
-    SourceInterface source;
-    
-    public Decorator(){
-        
-    }
-    
-    public Decorator(SourceInterface source){
-        this.source = source;
-    }
+public class Decorator implements SourceInterface {
+	SourceInterface source;
 
-    @Override
-    public void sayHello() {
-        source.sayHello();
-        System.out.println("this is Decorator , niubi bu ?");
-    }
-    
+	public Decorator() {
+
+	}
+
+	public Decorator(SourceInterface source) {
+		this.source = source;
+	}
+
+	@Override
+	public void sayHello() {
+		source.sayHello();
+		System.out.println("this is Decorator printing");
+	}
+
 }
